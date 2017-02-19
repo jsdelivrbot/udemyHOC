a18274394af0d8c0bed00b4460b953be5f8a98bc
Ted Werbel <werbel.ted@gmail.com>
2017-02-19 16:47:51 -0500

    Initial commit
    Added redux, react-router, and a higher order component. Connecting the authentication actions to the sign in button is next...

---
+++ src/components/resources.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <div>
+      Super Special Secret Recipe
+      <ul>
+        <li>meh</li>
+        <li>meh</li>
+        <li>meh</li>
+        <li>meh</li>
+        <li>meh</li>
+      </ul>
+    </div>
+  );
+};
